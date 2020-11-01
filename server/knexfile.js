f262a00c1f8ba94e022362e65d4ca6d9950e7153
Sean Kramer <seandominickramer@gmail.com>
2020-11-01 12:43:25 -0500

    Delete and redo data

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,12 +6,17 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/arguments.db3'
+    },
+    useNullAsDefault: true,
+
+    migrations: {
+      directory: './data/migrations'
+    },
+  
+    seeds: {
+      directory: './data/seeds'
     }
   },
-  useNullAsDefault: true,
-
-  migrations: {
-    directory: './data/migrations'
-  }
+ 
 
 };
